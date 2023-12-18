45813d7cad48a4646521af702ef4e813bd4b95e5
kmj101 <127805445+kmj101@users.noreply.github.com>
2023-12-18 13:46:49 +0900

    ㅇㄴ

--- script/sketch.js
+++ script/sketch.js
@@ -18,7 +18,7 @@ function setup() {
   textSize(20);
   // 텍스트의 색을 형광핑크로 변경
   fill('#FF00FF');
-  text('커서를 움직여보세요', width / 2, 30);
+  text('커서를 천천히 움직여보세요', width / 2, 30);
   text('r = 초기화, space = 일시정지', width / 2, 70);
 }
 
@@ -45,8 +45,8 @@ function keyPressed() {
   if (keyCode === 82) {
     background(0);
     fill('#FF00FF');
-    text('커서를 움직여보세요', width / 2, 30); // 상단으로 옮김
-    text('r = 초기화, space = 일시정지', width / 2, 70); // 상단으로 옮김
+    text('커서를 천천히 움직여보세요', width / 2, 30);
+    text('r = 초기화, space = 일시정지', width / 2, 70);
   } //'r'eset
   if (keyCode === 32) {
     running = !running;
